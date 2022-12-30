b0ad83be6ed63249a290b2f9b806907152e83c58
César M. Cristóbal <cesar@callepuzzle.com>
2022-12-30 11:16:50 +0100

    InputNames as component

---
+++ src/routes/InputNames.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import Textfield from '@smui/textfield';
+	import HelperText from '@smui/textfield/helper-text';
+	import Checkbox from '@smui/checkbox';
+	import FormField from '@smui/form-field';
+
+	let inputNames = '';
+	let inputNamesArray: string[] = [];
+	export let values: string[] = [];
+
+	function handleInputNames() {
+		inputNamesArray = inputNames.split('\n');
+		// Clear whitespace
+		inputNamesArray = inputNamesArray.map((name) => name.trim());
+		// Remove empty strings
+		inputNamesArray = inputNamesArray.filter((name) => name !== '');
+		// Remove duplicates
+		inputNamesArray = [...new Set(inputNamesArray)];
+	}
+</script>
+
+<div>
+	<Textfield textarea bind:value={inputNames} on:change={handleInputNames}>
+		<HelperText slot="helper">César Luis Juan Pedro</HelperText>
+	</Textfield>
+
+	<FormField>
+		{#each inputNamesArray as name}
+			<Checkbox {name} value={name} bind:group={values} />
+			<label for={name}>{name}</label>
+		{/each}
+	</FormField>
+</div>
